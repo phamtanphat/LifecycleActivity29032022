9b5e164c730efd4c49a57e559b330a9f86e4560d
Phat <phatdroid94@gmail.com>
2022-05-31 18:59:28 +0700

    single top

--- app/src/main/java/com/example/lifecycleactivity29032022/MainActivity2.java
+++ app/src/main/java/com/example/lifecycleactivity29032022/MainActivity2.java
@@ -2,13 +2,64 @@ package com.example.lifecycleactivity29032022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button btnNavigateScreen3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Log.d("BBB","Main2 : onCreate");
+        btnNavigateScreen3 = findViewById(R.id.button_navigate_screen3);
+
+        btnNavigateScreen3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("BBB","Main2 : onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("BBB","Main2 : onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("BBB","Main2 : onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("BBB","Main2 : onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("BBB","Main2 : onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("BBB","Main2 : onRestart");
     }
 }
--- app/src/main/java/com/example/lifecycleactivity29032022/MainActivity3.java
+++ app/src/main/java/com/example/lifecycleactivity29032022/MainActivity3.java
@@ -8,59 +8,56 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
-    Button btnNavigateScreen2;
+    Button btnNavigateScreen3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("BBB","Main1 : onCreate");
-
-        btnNavigateScreen2 = findViewById(R.id.button_navigate_screen2);
-
-        btnNavigateScreen2.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main3);
+        Log.d("BBB","Main3 : onCreate");
+        btnNavigateScreen3 = findViewById(R.id.button_navigate_screen2);
+        btnNavigateScreen3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                Intent intent = new Intent(MainActivity3.this, MainActivity3.class);
                 startActivity(intent);
             }
         });
     }
-
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("BBB","Main1 : onStart");
+        Log.d("BBB","Main3 : onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("BBB","Main1 : onResume");
+        Log.d("BBB","Main3 : onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("BBB","Main1 : onPause");
+        Log.d("BBB","Main3 : onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("BBB","Main1 : onStop");
+        Log.d("BBB","Main3 : onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("BBB","Main1 : onDestroy");
+        Log.d("BBB","Main3 : onDestroy");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d("BBB","Main1 : onRestart");
+        Log.d("BBB","Main3 : onRestart");
     }
 }
